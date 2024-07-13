6bca1c3c30d8659f327d3c971fc2dc2da9009411
sehoonk2004 <107429645+sehoonk2004@users.noreply.github.com>
2024-07-13 19:20:59 -0400

    Created master-doc.tsx file in components folder

--- app/master-doc/pages.tsx
+++ app/master-doc/pages.tsx
@@ -1,6 +1,17 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import MasterDocComponent from "@/components/master-doc";
+import { constructMetadata } from "@/lib/utils";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = constructMetadata({
+  title: "Master Doc", //potentially edit
+  description: "Master Doc PearAI", //potentially edit
+  canonical: "/master-doc", //potentially edit
+});
 
 export default function MasterDoc() {
-  return <></>;
+  return (
+    <>
+      <MasterDocComponent />
+    </>
+  );
 }
---
+++ components/master-doc.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
+
+const MasterDocComponent: React.FC = () => {
+  return <></>;
+};
+export default MasterDocComponent;
